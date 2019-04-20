3449e20e772282dc21b9fe515531c966ba17efc7
Martin Metaksov <info@metaksov.dk>
2019-04-20 09:53:10 +0200

    nlp cleanup; started on artificial neural networks

---
+++ NeuralNetworks/ArtificialNeuralNetworks/ann.py
@@ -0,0 +1,8 @@
+# %% Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# %%
+dataset = pd.read_csv(
+    'NeuralNetworks/ArtificialNeuralNetworks/.csv')
